0ec7c9802b6b76c9006fd44f4bf5c6465383b266
Haraade <feriemobiltelefon@gmail.com>
2020-03-22 21:05:15 +0100

    Update clk-exynos5420.c
    CPU overclocking

--- drivers/clk/samsung/clk-exynos5420.c
+++ drivers/clk/samsung/clk-exynos5420.c
@@ -1272,7 +1272,7 @@ static const struct samsung_gate_clock exynos5x_gate_clks[] __initconst = {
 };
 
 static const struct samsung_pll_rate_table exynos5420_pll2550x_24mhz_tbl[] __initconst = {
-	PLL_35XX_RATE(2000000000, 250, 3, 0),
+	PLL_35XX_RATE(2100000000, 175, 2, 0),
 	PLL_35XX_RATE(1900000000, 475, 6, 0),
 	PLL_35XX_RATE(1800000000, 225, 3, 0),
 	PLL_35XX_RATE(1700000000, 425, 6, 0),
@@ -1376,7 +1376,7 @@ static const struct exynos_cpuclk_cfg_data exynos5420_eglclk_d[] __initconst = {
 };
 
 static const struct exynos_cpuclk_cfg_data exynos5800_eglclk_d[] __initconst = {
-	{ 2000000, E5420_EGL_DIV0(3, 7, 7, 4), },
+	{ 2100000, E5420_EGL_DIV0(3, 7, 7, 4), },
 	{ 1900000, E5420_EGL_DIV0(3, 7, 7, 4), },
 	{ 1800000, E5420_EGL_DIV0(3, 7, 7, 4), },
 	{ 1700000, E5420_EGL_DIV0(3, 7, 7, 3), },
@@ -1402,7 +1402,7 @@ static const struct exynos_cpuclk_cfg_data exynos5800_eglclk_d[] __initconst = {
 		((((kpll) << 24) | ((pclk) << 20) | ((aclk) << 4)))
 
 static const struct exynos_cpuclk_cfg_data exynos5420_kfcclk_d[] __initconst = {
-	{ 1500000, E5420_KFC_DIV(3, 5, 3), },
+	{ 1550000, E5420_KFC_DIV(3, 5, 3), },
 	{ 1400000, E5420_KFC_DIV(3, 5, 3), }, /* for Exynos5800 */
 	{ 1300000, E5420_KFC_DIV(3, 5, 2), },
 	{ 1200000, E5420_KFC_DIV(3, 5, 2), },
