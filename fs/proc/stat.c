8ba852261dae2be5cc61a3bacfacbaba10b6d27f
Hanjie Lin <hanjie.lin@amlogic.com>
2023-04-19 17:41:08 +0800

    smp: add APU support [1/2]
    PD#SWPL-119162
    
    Problem:
    APU should not be seen except audio
    
    Solution:
    add APU support
    
    Verify:
    t5m
    
    Change-Id: I03828c60962a16cdf801c8ddabeb7e05b4a4f232
    Signed-off-by: Hanjie Lin <hanjie.lin@amlogic.com>

--- fs/proc/stat.c
+++ fs/proc/stat.c
@@ -100,6 +100,10 @@ static int show_stat(struct seq_file *p, void *v)
 		struct kernel_cpustat kcpustat;
 		u64 *cpustat = kcpustat.cpustat;
 
+#ifdef CONFIG_AMLOGIC_APU
+		if (apu_enable && i == apu_id)
+			continue;
+#endif
 		kcpustat_cpu_fetch(&kcpustat, i);
 
 		user		+= cpustat[CPUTIME_USER];
@@ -140,6 +144,10 @@ static int show_stat(struct seq_file *p, void *v)
 		struct kernel_cpustat kcpustat;
 		u64 *cpustat = kcpustat.cpustat;
 
+#ifdef CONFIG_AMLOGIC_APU
+		if (apu_enable && i == apu_id)
+			continue;
+#endif
 		kcpustat_cpu_fetch(&kcpustat, i);
 
 		/* Copy values here to work around gcc-2.95.3, gcc-2.96 */
--- include/linux/smp.h
+++ include/linux/smp.h
@@ -294,6 +294,12 @@ int smpcfd_prepare_cpu(unsigned int cpu);
 int smpcfd_dead_cpu(unsigned int cpu);
 int smpcfd_dying_cpu(unsigned int cpu);
 
+#ifdef CONFIG_AMLOGIC_APU
+extern int apu_id;
+extern int apu_hwid;
+extern int apu_enable;
+#endif
+
 #ifdef CONFIG_CSD_LOCK_WAIT_DEBUG
 bool csd_lock_is_stuck(void);
 #else
