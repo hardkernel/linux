cd2782fc4f2fd3458f913cf3834de4ce7ee8cf06
Bichao Zheng <bichao.zheng@amlogic.com>
2023-04-20 16:23:24 +0800

    nand: fix nand bugs [1/1]
    PD#SWPL-120491
    PD#SWPL-129754
    PD#SWPL-155674
    
    Problem:
    1.wrong select onfi mode
    2.erase flash error during OTA upgrade
    3.strncmp can't match full id 01h,DCh,00h,05h,04h(S34ML04G300BHI00)
    and 01h,DCh,00h,1Ah,00h(S34ML04G300TFI000)
    
    Solution:
    1.get onfi mode from id table
    2.change the check way of badblock
    3.use memcmp to compare ID memory instead strncmp
    
    Verify:
    1-2:s4
    
    Change-Id: Ie747ceb95f1045c2acaaf2afb6862c9b05d36d8b
    Signed-off-by: Bichao Zheng <bichao.zheng@amlogic.com>
    Signed-off-by: zhikui.cui <zhikui.cui@amlogic.com>

--- drivers/mtd/nand/raw/nand_base.c
+++ drivers/mtd/nand/raw/nand_base.c
@@ -967,6 +967,10 @@ int nand_choose_best_sdr_timings(struct nand_chip *chip,
 	return ret;
 }
 
+#if IS_ENABLED(CONFIG_AMLOGIC_MTD_NAND)
+EXPORT_SYMBOL_GPL(nand_choose_best_sdr_timings);
+#endif
+
 /**
  * nand_choose_best_nvddr_timings - Pick up the best NVDDR timings that both the
  *                                  NAND controller and the NAND chip support
@@ -5086,7 +5090,11 @@ static bool find_full_id_nand(struct nand_chip *chip,
 
 	memorg = nanddev_get_memorg(&chip->base);
 
+#if IS_ENABLED(CONFIG_AMLOGIC_MTD_NAND)
+	if (!memcmp(type->id, id_data, type->id_len)) {
+#else
 	if (!strncmp(type->id, id_data, type->id_len)) {
+#endif
 		memorg->pagesize = type->pagesize;
 		mtd->writesize = memorg->pagesize;
 		memorg->pages_per_eraseblock = type->erasesize /
@@ -5409,7 +5417,9 @@ ident_done:
 		chip->options |= NAND_ROW_ADDR_3;
 
 	chip->badblockbits = 8;
-
+#if IS_ENABLED(CONFIG_AMLOGIC_MTD_NAND)
+	chip->type = type;
+#endif
 	nand_legacy_adjust_cmdfunc(chip);
 
 	pr_info("device found, Manufacturer ID: 0x%02x, Chip ID: 0x%02x\n",
--- drivers/mtd/nand/raw/nand_bbt.c
+++ drivers/mtd/nand/raw/nand_bbt.c
@@ -75,9 +75,16 @@
 
 static inline uint8_t bbt_get_entry(struct nand_chip *chip, int block)
 {
+#if !IS_ENABLED(CONFIG_AMLOGIC_MTD_NAND)
 	uint8_t entry = chip->bbt[block >> BBT_ENTRY_SHIFT];
 	entry >>= (block & BBT_ENTRY_MASK) * 2;
 	return entry & BBT_ENTRY_MASK;
+#else
+	if (chip->bbt)
+		return	chip->bbt[block];
+
+	return 1;
+#endif
 }
 
 static inline void bbt_mark_entry(struct nand_chip *chip, int block,
@@ -1457,6 +1464,7 @@ int nand_isbad_bbt(struct nand_chip *this, loff_t offs, int allowbbt)
 	if (mtd_check_expert_analysis_mode())
 		return 0;
 
+#if !IS_ENABLED(CONFIG_AMLOGIC_MTD_NAND)
 	switch (res) {
 	case BBT_BLOCK_GOOD:
 		return 0;
@@ -1466,6 +1474,9 @@ int nand_isbad_bbt(struct nand_chip *this, loff_t offs, int allowbbt)
 		return allowbbt ? 0 : 1;
 	}
 	return 1;
+#else
+	return res ? 1 : 0;
+#endif
 }
 
 /**
