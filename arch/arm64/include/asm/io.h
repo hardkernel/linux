e51d8c3e9e3de696b743cdb8d2aa1a50f2bc69ba
qiankun.wang <qiankun.wang@amlogic.com>
2023-05-11 15:08:24 +0800

    iotrace: support iotrace driver [1/1]
    PD#SWPL-114632
    
    Problem:
    support iotrace driver
    
    Solution:
    support iotrace
    
    Verify:
    on s4d_ap222 and c2_af400
    
    Change-Id: Id4dc4f64d886dc551f32d776ebaa2c9c2d5650b0
    Signed-off-by: qiankun.wang <qiankun.wang@amlogic.com>

--- arch/arm64/include/asm/io.h
+++ arch/arm64/include/asm/io.h
@@ -18,6 +18,12 @@
 #include <asm/alternative.h>
 #include <asm/cpufeature.h>
 
+/* builtin code include iotrace can cause some symbols crc changed */
+#if IS_BUILTIN(CONFIG_AMLOGIC_DEBUG_IOTRACE)
+#include <linux/amlogic/io_64.h>
+#elif (IS_MODULE(CONFIG_AMLOGIC_DEBUG_IOTRACE)) && defined(MODULE)
+#include <linux/amlogic/io_64.h>
+#else
 /*
  * Generic IO read/write.  These perform native-endian accesses.
  */
@@ -93,6 +99,7 @@ static __always_inline u64 __raw_readq(const volatile void __iomem *addr)
 		     : "=r" (val) : "r" (addr));
 	return val;
 }
+#endif
 
 /* IO barriers */
 #define __io_ar(v)							\
