f2e4d7943ed3a98708f3cf299e79e34d66afd7a5
Dongjin Kim <tobetter@gmail.com>
2023-08-01 16:39:22 +0900

    ODROID-M1: drm/panel: add new panel compatible 'odroid,vu8m'
    Signed-off-by: Dongjin Kim <tobetter@gmail.com>
    Change-Id: I841191641d9d0218b3881cf5b802904c751fa8f5

--- drivers/gpu/drm/panel/panel-ilitek-ili9881c.c
+++ drivers/gpu/drm/panel/panel-ilitek-ili9881c.c
@@ -1197,6 +1197,7 @@ static const struct of_device_id ili9881c_of_match[] = {
 	{ .compatible = "feixin,k101-im2byl02", .data = &k101_im2byl02_desc },
 	{ .compatible = "wanchanglong,w552946aba", .data = &w552946aba_desc },
 	{ .compatible = "odroid,hj080be31ia1", .data = &odroid_hj080be31ia1_desc },
+	{ .compatible = "odroid,vu8m", .data = &odroid_hj080be31ia1_desc },
 	{ }
 };
 MODULE_DEVICE_TABLE(of, ili9881c_of_match);
